3e671ba019e10f96747fa12b594387c5587367fd
mmzahed92 <42880801+mmzahed92@users.noreply.github.com>
2020-03-02 22:22:36 -0800

    Homepage with login and signup

---
+++ HomePage/HomePage/DesignableView.swift
@@ -0,0 +1,34 @@
+//
+//  DesignableView.swift
+//  HomePage
+//
+//  Created by Mohammad Zahed on 3/2/20.
+//  Copyright © 2020 mac mojave. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear{
+        didSet{
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet{
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet{
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet{
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }}
